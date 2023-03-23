01872bdfbb473e1fa0ca05d199b09a0411d620f0
Manu Rufener <rufenermanuel@gmail.com>
2023-03-22 22:04:14 -0300

    se agregó filtro por rol al componente NewUsersForm.js y se corrigió doble renderizado

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import "./App.css";
 import NewUsersForm from "./components/forms/NewUsersForm";
 
 function App() {
-  return <p className="unica">Todo está bien!</p>;
-  <NewUsersForm/>
+  return <NewUsersForm />;
 }
 
 export default App;
--- src/components/forms/NewUsersForm.js
+++ src/components/forms/NewUsersForm.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
-
+import React, {  useState } from "react";
 const NewUsersForm = (props) => {
-  const [userData, setUserData] = useState({
+/*   const [userData, setUserData] = useState({
     Name: "",
     LastName: "",
     Dni: "",
@@ -14,121 +13,176 @@ const NewUsersForm = (props) => {
     NumeroAfiliado: "",
     Cud: "",
   });
+ */
+  const [displayProfesional, setDisplayProfesional] = useState('none');
+  const [displayPaciente, setDisplayPaciente] = useState('none');
+  const [displayAdministrativo, setDisplayAdministrativo] = useState('none')
+
+  const handleSelectChangeRoles = (event) => {
+console.log(event.target.value)
+    if (event.target.value === 'profesional') {
+      setDisplayProfesional('block');
+      setDisplayPaciente('none');
+      setDisplayAdministrativo('none')
+    } else if (event.target.value === 'paciente') {
+      console.log('entré a paciente')
+      setDisplayPaciente('block');
+      setDisplayProfesional('none');
+      setDisplayAdministrativo('none')
+    } else {
+      setDisplayAdministrativo('block')
+      setDisplayPaciente('none');
+      setDisplayProfesional('none');
+    }
+    
+  }
 
   return (
     <div>
       <h1 id="finish">Registre Nuevo Usuario</h1>
       <form>
-        <div>
-          <p>Nombre/es</p>
-          <input
-            type="text"
-            placeholder="Nombre"
-            className="form"
-            name="Name"
-            id="input-Id"
-          ></input>
+        <div id="containerDataBase">
+          <div>
+            <p>Nombre/es</p>
+            <input
+              type="text"
+              placeholder="Nombre"
+              className="form"
+              name="Name"
+              id="input-Id"
+            ></input>
+          </div>
+          <div>
+            <p>Apellido/os</p>
+            <input
+              type="text"
+              placeholder="Apellido/os"
+              className="form"
+              name="LastName"
+              id="input-Id"
+            ></input>
+          </div>
+          <div>
+            <p>DNI</p>
+            <input
+              type="Number"
+              placeholder="DNI"
+              className="form"
+              name="Dni"
+              id="input-Id"
+            ></input>
+          </div>
+          <div>
+            <p>Email</p>
+            <input
+              type="email"
+              placeholder="email"
+              className="form"
+              name="Email"
+              id="input-Id"
+            ></input>
+          </div>
+          <div>
+            <p>Celular</p>
+            <input
+              type="Number"
+              placeholder="cellphone"
+              className="form"
+              name="Cellphone"
+              id="input-Id"
+            ></input>
+          </div>
+          <div>
+            <p>Teléfono</p>
+            <input
+              type="Number"
+              placeholder="phone"
+              className="form"
+              name="Phone"
+              id="input-Id"
+            ></input>
+          </div>
+          <div>
+            <p>Rol</p>
+            <select name="Rol" id="input-Id" onChange={handleSelectChangeRoles}>
+              <option value="">Selecciona un rol</option>
+              <option value="paciente">Paciente</option>
+              <option value="profesional">Profesional</option>
+              <option value="administrativo">Administrativo</option>
+            </select>
+          </div>
         </div>
-        <div>
-          <p>Apellido/os</p>
-          <input
-            type="text"
-            placeholder="Apellido/os"
-            className="form"
-            name="LastName"
-            id="input-Id"
-          ></input>
-        </div>
-        <div>
-          <p>DNI</p>
-          <input
-            type="Number"
-            placeholder="DNI"
-            className="form"
-            name="Dni"
-            id="input-Id"
-          ></input>
-        </div>
-        <div>
-          <p>Email</p>
-          <input
-            type="email"
-            placeholder="email"
-            className="form"
-            name="Email"
-            id="input-Id"
-          ></input>
-        </div>
-        <div>
-          <p>Celular</p>
-          <input
-            type="Number"
-            placeholder="cellphone"
-            className="form"
-            name="Cellphone"
-            id="input-Id"
-          ></input>
-        </div>
-        <div>
-          <p>Teléfono</p>
-          <input
-            type="Number"
-            placeholder="phone"
-            className="form"
-            name="Phone"
-            id="input-Id"
-          ></input>
-        </div>
-        <div>
-          <p>Rol</p>
-          <select name="Rol" id="input-Id">
-            <option value="">Selecciona un rol</option>
-            <option value="1">Paciente</option>
-            <option value="2">Profesional Prestador</option>
-            <option value="3">Administrativo</option>
-          </select>
-        </div>
-        <div>
-          <p>Terapias</p>
-          <p>En el caso de necesitar marcar más de una manten presionada la tecla CTRL</p>
+        <div id="containerProfesional" style={{ display: displayProfesional }}>
+          <div>
+            <p>Especialidad</p>
+            <select name="Especialidad" id="input-Id">
+              <option value="">Elija una especialidad</option>
+              <option value="Fonoaudiología">Fonoaudiología</option>
+              <option value="Psicopedagogía">Psicopedagogía</option>
+              <option value="Psicomotricidad">Psicomotricidad</option>
+            </select>
+            <p>Terapias ofrecidas</p>
+            <p>
+              En el caso de necesitar marcar más de una manten presionada la
+              tecla CTRL
+            </p>
 
-          <select name="Terapias" id="input-Id" multiple>
-            <option value="1">Terapia 1</option>
-            <option value="2">Terapia 2</option>
-            <option value="3">Terapia 3</option>
-          </select>
-         
+            <select name="Terapias" id="input-Id" multiple>
+              <option value="1">Terapia 1</option>
+              <option value="2">Terapia 2</option>
+              <option value="3">Terapia 3</option>
+            </select>
+          </div>
         </div>
-        <div>
-          <p>Obra Social</p>
-          <select name="ObraSocial" id="input-Id">
-            <option value="">Selecciona una Obra Social</option>
-            <option value="1">Option 1</option>
-            <option value="2">Option 2</option>
-            <option value="3">Option 3</option>
-          </select>
+        <div id="containerPaciente" style={{ display: displayPaciente }}>
+          <div>
+            <div>
+              <p>Obra Social</p>
+              <select name="ObraSocial" id="input-Id">
+                <option value="">Selecciona una Obra Social</option>
+                <option value="1">Option 1</option>
+                <option value="2">Option 2</option>
+                <option value="3">Option 3</option>
+              </select>
+            </div>
+            <div>
+              <p>Número Afiliado</p>
+              <input
+                type="Number"
+                placeholder="Numero Afiliado"
+                className="form"
+                name="NumeroAfiliado"
+                id="input-Id"
+              ></input>
+            </div>
+            <div>
+              <p>Número CUD</p>
+              <input
+                type="Number"
+                placeholder="Numero Certificado único discapacidad"
+                className="form"
+                name="Cud"
+                id="input-Id"
+              ></input>
+
+              <p>Terapias</p>
+              <p>
+                En el caso de necesitar marcar más de una manten presionada la
+                tecla CTRL
+              </p>
+
+              <select name="Terapias" id="input-Id" multiple>
+                <option value="1">Terapia 1</option>
+                <option value="2">Terapia 2</option>
+                <option value="3">Terapia 3</option>
+              </select>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>Número Afiliado</p>
-          <input
-            type="Number"
-            placeholder="Numero Afiliado"
-            className="form"
-            name="NumeroAfiliado"
-            id="input-Id"
-          ></input>
-        </div>
-        <div>
-          <p>Número CUD</p>
-          <input
-            type="Number"
-            placeholder="Numero Certificado único discapacidad"
-            className="form"
-            name="Cud"
-            id="input-Id"
-          ></input>
-        </div>
+        <div id="containerAdminstrativo" style={{ display: displayAdministrativo }}></div>
+
       </form>
+
       <div>
         <button>Agregar</button>
       </div>
